3c88ffc1fad22897ffbe9229e34c51deda134918
TusharSahaTonoy <tonoysh@gmail.com>
2022-06-06 17:22:47 +0600

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import Home from './pages/home/Home';
+
+import './App.css';
+import './utilities/common.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/home/Home.js
@@ -0,0 +1,16 @@
+import Header from "../../components/template/header/header";
+import Footer from "../../components/template/footer/footer";
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <div>
+                hello
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
