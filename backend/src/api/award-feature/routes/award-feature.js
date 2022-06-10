5902d55057e70675d2f03bb663aae6f2aee7f44b
TusharSahaTonoy <tonoysh@gmail.com>
2022-06-10 19:04:32 +0600

    strapi added in backend

---
+++ backend/src/api/award-feature/routes/award-feature.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * award-feature router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::award-feature.award-feature');
---
+++ backend/src/api/award-feature/services/award-feature.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * award-feature service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::award-feature.award-feature');
